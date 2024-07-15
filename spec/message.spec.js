c58dc8c2645dafc1f776bc79a3b63988fd293922
NickWeller <nickweller2@gmail.com>
2024-07-14 19:37:55 -0500

    test

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,21 @@
-const Message = require('../message.js');
-const Command = require('../command.js');
+const Message = require("../message.js");
+const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
-
+describe("Message class", function () {
+  it("throws an error if a name is not passed into the constructor as the first parameter", function () {
+    expect(function () {
+      new Message();
+    }).toThrow(new Error("Message name required."));
+  });
+  test("constructor sets name", function () {
+    let result = new Message("Message");
+    expect(result.name).toBe("Message");
+  });
+  test("contains a commands array passed into the constructor as the 2nd argument", function () {
+    let result = new Message("Message", "Commands");
+    expect(result.commands).toBe("Commands");
+  });
 });
